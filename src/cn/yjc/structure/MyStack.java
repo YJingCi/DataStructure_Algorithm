a50d89e7c18ee9990d31b2ec10a4d46de7c0de74
yaowen <yw43194@ly.com>
2017-09-18 18:51:06 +0800

    初始化栈

---
+++ src/cn/yjc/structure/MyStack.java
@@ -0,0 +1,49 @@
+package cn.yjc.structure;
+
+
+public class MyStack {
+
+	//入栈操作
+	public void push(Object obj){
+		
+	}
+	
+	//出栈操作
+	public Object pop(){
+		return null;
+	}
+	
+	//查看栈顶元素
+	public Object peek(){
+		return null;
+	}
+	
+	
+	
+	//该栈是否为空
+	public boolean isEmpty(){
+		return true;
+	}
+	
+	//清除栈中的所有内容
+	public void clear(){
+		
+	}
+	
+	//返回栈的长度
+	public int size(){
+		return 0;
+	}
+
+	
+	/**
+	 * 这个方法不是栈本身的方法，但是我们常常重写 toString方法，是为了便于打印。
+	 */
+	@Override
+	public String toString() {
+		return "";
+	}
+	
+	
+	
+}
